b40e0a41a71aedf68ea4256315e8b78adca6fb90
danieldave <danielolusoji@gmail.com>
2025-09-08 14:30:37 +0100

    Internal Error Not Fixed Earlier

--- bonus_app/urls.py
+++ bonus_app/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from .views import dashboard
+from django.urls import include, path
+from bonuses import views   # 👈 this was missing
+
 
 
 urlpatterns = [
