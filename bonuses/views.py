619ef99f8821371e456fe911f7c5993cfcad6be7
danieldave <danielolusoji@gmail.com>
2025-09-09 06:11:46 +0100

    Register

--- bonuses/views.py
+++ bonuses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, "home.html")
@@ -23,3 +24,25 @@ def register(request):
         form = UserCreationForm()
     return render(request, "register.html", {"form": form})
 
+
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+
+
+def home(request):
+    return render(request, "home.html")
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # auto login after register
+            return redirect("home")
+    else:
+        form = UserCreationForm()
+    return render(request, "register.html", {"form": form})
